8c9cb102002c51ba840e7ddb5c7559f08b726e28
PauloEduardoMartins <pauloeduardomartinsdutra@gmail.com>
2024-06-20 16:47:05 -0300

    correções

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const options={
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'https://trabalho-marcio-6me3d4x3k-pauloeduardomartins-projects.vercel.app/api-docs/',
             },
         ],
     },
